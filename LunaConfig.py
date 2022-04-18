5e529916a2526580216a94bf84f275cf285a6c69
MKfathom <103910091+MKfathom@users.noreply.github.com>
2022-04-17 21:28:00 -0400

    Update LunaConfig.py

--- LunaConfig.py
+++ LunaConfig.py
@@ -1,5 +1,5 @@
-# replace "paste here 1" with your bot token
-# replace "paste here 2" with your discord username Example: Smug#6469
+# replace OTY1NDE3MzEyMTg4NTAyMDE2.Yly47w.vLs8mr6VLgWcAT0A37X86TS7L6M with your bot token
+# replace Breadwinner with your discord username Example: Smug#6469
 
 token = "paste here 1"
 username = "paste hear 2"
